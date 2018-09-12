522b6ff8bfaecacf4886750655ae5ca0ea7a7c70
SAaronB <programmingsean@gmail.com>
2018-09-12 08:08:32 -0600

    Added Getters and Setters to chang diffrent attributes of monster

--- src/Model/MarshmallowMonster.java
+++ src/Model/MarshmallowMonster.java
@@ -19,7 +19,6 @@ public class MarshmallowMonster
 		
 	}
 	
-	
 	public MarshmallowMonster(String name) {
 		this.name = name;
 	}
@@ -32,10 +31,64 @@ public class MarshmallowMonster
 		this.hasNoses = hasNoses;
 		this.armCount = armCount;
 	}
-	
-	
 //--------------CONSTRUCTOR SECTION---------------	
 	
+	
+//--------------------Getter----------------------
+//makes all the data members content public
+	
+	public String getName() 
+	{
+		return name;
+	}
+
+	public double getLegCount()
+	{
+		return legCount;
+	}
+	
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+	
+	public boolean getHasNoses()
+	{
+		return hasNoses;
+	}
+	
+	public int getArmCount()
+	{
+		return armCount;
+	}
+//----------------------Getter--------------------
+	
+	
+//-------------------Setters----------------------
+	
+	public void setName(String name)
+	{
+		//this is reffering to the arrgument
+		this.name = name;
+	}
+	
+	public void setLegCount(double legs) 
+	{
+		this.legCount = legs;
+	}
+	
+	public void setEyeCount(int eyes) {
+		this.eyeCount = eyeCount;
+	}
+	
+	public void setHasNoses(boolean hasNoses) {
+		this.hasNoses = hasNoses;
+	}
+	
+	public void setArmCount(int arms) {
+		this.armCount = arms;
+	}
+//-------------------Setters-----------------------
 	
 	
 	public String toString() 
