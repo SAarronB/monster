1eabeecb15f0266c57438fafb3370991217c1f73
SAaronB <programmingsean@gmail.com>
2018-09-14 08:28:16 -0600

    Took the input from the JOptionPane.showInputDialog and added Try methods.

--- src/controller/MonsterController.java
+++ src/controller/MonsterController.java
@@ -39,24 +39,53 @@ public class MonsterController
 		
 		
 //-----------------------------------User Monster Output-------------------------------------
-		JOptionPane.showInputDialog(null, "What is your monsters name?");
+		String userMonsterName = JOptionPane.showInputDialog(null, "What is your monsters name?");
 //This is scanning any input and will take it in
-		Scanner inputScanner = new Scanner(System.in);
-		String nameAnswer = inputScanner.nextLine();
+		//Scanner inputScanner = new Scanner(System.in);
+		//String nameAnswer = inputScanner.nextLine();
 //Printing out the answer that was taken
-		JOptionPane.showMessageDialog(null, "Your monsters name is: " + nameAnswer);
+		JOptionPane.showMessageDialog(null, "Your monsters name is: " + userMonsterName);
 		
 		
-		JOptionPane.showInputDialog(null,"How many arms does your monster have?");
+		String userMonsterArms = JOptionPane.showInputDialog(null,"How many arms does your monster have?");
 //This is scanning any input and will take it in
-		Scanner inputScanner2 = new Scanner(System.in);
-		String armAnswer = inputScanner2.nextLine();
+		//Scanner inputScanner2 = new Scanner(System.in);
+		//String armAnswer = inputScanner2.nextLine();
 //Printing out the answer that was taken
-		JOptionPane.showMessageDialog(null, "Your monster has " + armAnswer + "arms");
-		JOptionPane.showMessageDialog(null, "You Created a Monster! \n" + "It has " + armAnswer + " arms" + " and is named " + nameAnswer);
-		inputScanner.close();
-		inputScanner2.close();
+		JOptionPane.showMessageDialog(null, "Your monster has " + userMonsterArms + " arms");
+		JOptionPane.showMessageDialog(null, "You Created a Monster! \n" + "It has "
+		+ userMonsterArms + " arms" + " and is named " + userMonsterName);
+//		inputScanner.close();
+//		inputScanner2.close();
 //-----------------------------------User Monster Output-------------------------------------
+	}
+	
+	public boolean validInt(String maybeInt) 
+	{
+		boolean isValid = false;
+		
+		try{
+			Integer.parseInt(maybeInt);
+			isValid = true;
+		}catch (NumberFormatException error){
+			JOptionPane.showMessageDialog(null, "The Try Didint work, type an Int Value");
+		}
+		
+		return isValid;
+	}
+	
+	public boolean validDouble(String maybeDouble) 
+	{
+		boolean isValid = false;
+		
+		try {
+			Double.parseDouble(maybeDouble);
+			isValid = true;
+		
+		return isValid;
+		} catch(NumberFormatExeption error){
+			JOption.showMessageDiolag(null, "The Double Try didnt work, input a Double.")
+		}
 	}
 //---------------METHOD SECTION-------------------
 }
